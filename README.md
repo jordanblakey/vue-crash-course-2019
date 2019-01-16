4508ce30505e88fcbc0aeb1d96ef6815597d95e4
Jordan Blakey <jordan@pulsem.me>
2019-01-15 21:42:36 -0600

    Project setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue Crash Course 2019
+
+```sh
+brew install node
+brew install yarn
+yarn global add @vue/cli # Install Vue CLI 3
+vue --version
+vue create test
+open http://localhost:8080
+vue ui
+```
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      msg: "Hello"
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
